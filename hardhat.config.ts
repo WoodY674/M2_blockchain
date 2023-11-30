34c0372afbc9a4706106ae65117e454f1ba36999
Antoine <antoine-haller@outlook.fr>
2023-11-30 20:19:26 +0100

    [FEAT] SMART CONTRACT :
    * fix config
    * tests ok
    * deploy smart contract done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,17 +8,17 @@ const config: HardhatUserConfig = {
 };
 
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
   networks: {
     hardhat: {
     },
-    polygon_mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
+    mumbai:{
+      chainId:80001,
+      accounts: [process.env.PRIVATE_KEY],
+      thrownOnCallFailures:true,
+      thrownOnTransactionFailures:true,
+      allowUnlimitedContractSize: true,
+      url:`https://rpc-mumbai.maticvigil.com`
     }
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
   },
   solidity: {
     version: "0.8.9",
